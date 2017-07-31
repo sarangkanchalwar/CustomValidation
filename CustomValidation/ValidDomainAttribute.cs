f2eb45ba83f08632f2e98cbe000c89255fa06bcf
SarangK <sarang@itmystix.com>
2017-07-31 21:01:27 +0530

    Added project to repository with some scratch code for validation attributes

---
+++ CustomValidation/ValidDomainAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CustomValidation
+{
+    public class ValidDomainAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var domain = value.ToString();
+            if (!domain.Contains('.'))
+                return new ValidationResult("Invalid domain name.");
+
+            var charCount = Regex.Matches(domain, @"[a-z]").Count;
+            var periodCount = Regex.Matches(domain, @"[.]").Count;
+            if (domain.Length != (charCount + periodCount))
+                return new ValidationResult("Invalid domain name.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ CustomValidation/ValidEmailAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace CustomValidation
+{
+    public class ValidEmailAttribute : System.ComponentModel.DataAnnotations.ValidationAttribute
+    {
+        /// <summary>
+        /// Returns true if email is valid.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <param name="validationContext">The context information about the validation operation.</param>
+        /// <returns>
+        /// An instance of the <see cref="T:System.ComponentModel.DataAnnotations.ValidationResult" /> class.
+        /// </returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null) return new ValidationResult("Email address required.");
+            try
+            {
+                var mail = new MailAddress(value.ToString());
+                return ValidationResult.Success;
+            }
+            catch (Exception)
+            {
+                return new ValidationResult("Invalid email address.");
+            }
+        }
+    }
+}
